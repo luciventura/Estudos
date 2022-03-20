1dd58becb8445d05364f15043e2a206d9c7ae515
Luciene Ventura <lucieneventura13@gmail.com>
2022-03-20 00:42:04 -0300

    Initial Commit

---
+++ Animations.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/*
+ Animações devem direcionar a atenção do usuário, manter o usuário orientado ou se conectar ao comportamento do usuário.
+ 
+ Atenção: deixar o usuário ciente de que algo mudou no app ou atrair a atenção dele para uma nova feature ou área da tela.
+ Exemplo: mensagem de alerta "pulando" na tela, setas orientando no onboarding de jogos, notificação subindo no ícone do app na home screen.
+ 
+ 
+ Orientação: O usuário precisa saber de onde ele veio e pra onde ele vai.
+ Exemplo: quando você está vendo uma imagem em tela cheia no instagram, desliza pra baixo e ve ela diminuindo até voltar pra miniatura; Quando tocamos em uma pasta no home screen, ela "levanta" os ícones para mostrar os apps. Tocando no app, podemos ver ele aumentando de tamanho. Conseguimos acompanhar o percurso do abrir e fechar o app e sabemos sua localização através dessa animação.
+ 
+ 
+ Comportamento: O usuário precisa saber como seus gestos afetam o app para entender quais ações e comportamentos têm as respostas desejadas.
+ Exemplo: O deslizar da Notification Center. Nesse deslizar o usuário sabe que quando puxa do topo da tela, ele consegue ler as suas notificações, e tbm que pode deslizar pra cima e remover essa tela.
+ 
+ 
+ */
